2434947cc635fb8dc6efa7214aabc8e050a255e2
leohc87 <38429138+leohc87@users.noreply.github.com>
2020-11-30 18:40:44 -0300

    Add files via upload

---
+++ object.js
@@ -0,0 +1,16 @@
+let user = {
+    name: 'Guilherme'
+};
+
+// alternando a propiedade de um objeto
+user.name = 'Outro nome 1';
+user['name'] = 'Outro nome 2';
+
+const prop = 'name';
+user['prop'] = 'Outro nome 3';
+
+//Criando uma propiedade
+user.lastName = 'Cabrini da Silva';
+
+//Deletando uma propiedade
+delete user.name;
